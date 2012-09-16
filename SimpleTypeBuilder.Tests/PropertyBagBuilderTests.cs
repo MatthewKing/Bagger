8e95964e91761839f109f3210f157bc4137f5b13
Matthew King <mking@mking.info>
2012-09-16 21:40:03 +0800

    Implementation of the Build() method.

--- SimpleTypeBuilder.Tests/PropertyBagBuilderTests.cs
+++ SimpleTypeBuilder.Tests/PropertyBagBuilderTests.cs
@@ -66,6 +66,17 @@
         }
 
         [Test]
+        public void AddProperty_PropertyAlreadyExists_ThrowsArgumentException()
+        {
+            PropertyBagBuilder builder = new PropertyBagBuilder("name");
+            builder.AddProperty("Property", typeof(string));
+
+            Assert.That(() => builder.AddProperty("Property", typeof(string)),
+                Throws.TypeOf<ArgumentException>()
+                      .And.Message.Contains("Property 'Property' is already defined."));
+        }
+
+        [Test]
         public void AddProperty_ReturnsPropertyBagBuilderInstance()
         {
             PropertyBagBuilder builder = new PropertyBagBuilder("name");
@@ -73,6 +84,135 @@
             PropertyBagBuilder returnValue = builder.AddProperty("name", typeof(string));
 
             Assert.That(returnValue, Is.EqualTo(builder));
+        }
+
+        [Test]
+        public void CreatedType_ContainsExpectedProperties()
+        {
+            PropertyBagBuilder builder = new PropertyBagBuilder("name");
+            builder.AddProperty("Property1", typeof(string));
+            builder.AddProperty("Property2", typeof(int));
+
+            Type type = builder.Build();
+
+            Assert.That(type.GetProperty("Property1"), Is.Not.Null);
+            Assert.That(type.GetProperty("Property1").PropertyType, Is.EqualTo(typeof(string)));
+            Assert.That(type.GetProperty("Property2"), Is.Not.Null);
+            Assert.That(type.GetProperty("Property2").PropertyType, Is.EqualTo(typeof(int)));
+        }
+
+        [Test]
+        public void CreatedType_CanGetAndSetPropertyAsExpected()
+        {
+            PropertyBagBuilder builder = new PropertyBagBuilder("name");
+            builder.AddProperty("Property", typeof(string));
+
+            Type type = builder.Build();
+            PropertyInfo property = type.GetProperty("Property");
+            object instance = Activator.CreateInstance(type);
+
+            property.SetValue(instance, "Value", null);
+            object value = property.GetValue(instance, null);
+            Assert.That(value, Is.EqualTo("Value"));
+        }
+
+        [Test]
+        public void CreatedType_SetPropertyToIncompatibleType_ThrowsArgumentException()
+        {
+            PropertyBagBuilder builder = new PropertyBagBuilder("name");
+            builder.AddProperty("Property", typeof(string));
+
+            Type type = builder.Build();
+            PropertyInfo property = type.GetProperty("Property");
+            object instance = Activator.CreateInstance(type);
+
+            string message = "Object of type 'System.Object' cannot be converted to type 'System.String'.";
+            Assert.That(() => property.SetValue(instance, new object(), null),
+                Throws.TypeOf<ArgumentException>()
+                      .And.Message.Contains(message));
+        }
+
+        [Test]
+        public void CreatedType_ImplementsIPropertyBag()
+        {
+            PropertyBagBuilder builder = new PropertyBagBuilder("name");
+            Type type = builder.Build();
+
+            Assert.That(typeof(IPropertyBag).IsAssignableFrom(type), Is.True);
+        }
+
+        [Test]
+        public void CreatedType_AsIPropertyBag_GetValue_PropertyNameIsNotValid_ThrowsArgumentException()
+        {
+            PropertyBagBuilder builder = new PropertyBagBuilder("name");
+            builder.AddProperty("Property", typeof(string));
+
+            Type type = builder.Build();
+            IPropertyBag bag = Activator.CreateInstance(type) as IPropertyBag;
+
+            string message = "propertyName must refer to a parameter that exists on this type.";
+            Assert.That(() => bag.GetValue("Name"),
+                Throws.TypeOf<ArgumentException>()
+                      .And.Message.Contains(message));
+        }
+
+        [Test]
+        public void CreatedType_AsIPropertyBag_GetValue_ReturnsPropertyValue()
+        {
+            PropertyBagBuilder builder = new PropertyBagBuilder("name");
+            builder.AddProperty("Property", typeof(string));
+
+            Type type = builder.Build();
+            IPropertyBag bag = Activator.CreateInstance(type) as IPropertyBag;
+
+            PropertyInfo propertyInfo = type.GetProperty("Property");
+            propertyInfo.SetValue(bag, "expected_value", null);
+
+            Assert.That(bag.GetValue("Property"), Is.EqualTo("expected_value"));
+        }
+
+        [Test]
+        public void CreatedType_AsIPropertyBag_SetValue_PropertyNameIsNotValid_ThrowsArgumentException()
+        {
+            PropertyBagBuilder builder = new PropertyBagBuilder("name");
+            builder.AddProperty("Property", typeof(string));
+
+            Type type = builder.Build();
+            IPropertyBag bag = Activator.CreateInstance(type) as IPropertyBag;
+
+            string message = "propertyName must refer to a parameter that exists on this type.";
+            Assert.That(() => bag.SetValue("Name", new object()),
+                Throws.TypeOf<ArgumentException>()
+                      .And.Message.Contains(message));
+        }
+
+        [Test]
+        public void CreatedType_AsIPropertyBag_SetValue_ObjectIsOfInvalidType_ThrowsInvalidCastException()
+        {
+            PropertyBagBuilder builder = new PropertyBagBuilder("name");
+            builder.AddProperty("Property", typeof(string));
+
+            Type type = builder.Build();
+            IPropertyBag bag = Activator.CreateInstance(type) as IPropertyBag;
+
+            string message = "Unable to cast object of type 'System.Object' to type 'System.String'.";
+            Assert.That(() => bag.SetValue("Property", new object()),
+                Throws.TypeOf<InvalidCastException>()
+                      .And.Message.Contains(message));
+        }
+
+        [Test]
+        public void CreatedType_AsIPropertyBag_SetValue_SetsPropertyValue()
+        {
+            PropertyBagBuilder builder = new PropertyBagBuilder("name");
+            builder.AddProperty("Property", typeof(string));
+
+            Type type = builder.Build();
+            IPropertyBag bag = Activator.CreateInstance(type) as IPropertyBag;
+            bag.SetValue("Property", "expected_value");
+
+            PropertyInfo propertyInfo = type.GetProperty("Property");
+            Assert.That(propertyInfo.GetValue(bag, null), Is.EqualTo("expected_value"));
         }
     }
 }
--- SimpleTypeBuilder/PropertyBagBuilder.cs
+++ SimpleTypeBuilder/PropertyBagBuilder.cs
@@ -2,6 +2,8 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Reflection;
+    using System.Reflection.Emit;
 
     public sealed class PropertyBagBuilder
     {
@@ -32,18 +34,158 @@
             if (type == null)
                 throw new ArgumentNullException("type", "type should not be null.");
 
-            PropertyDefinition property = new PropertyDefinition(name, type);
+            foreach (PropertyDefinition property in this.properties)
+            {
+                if (String.Equals(name, property.Name))
+                {
+                    string message = String.Format("Property '{0}' is already defined.", name);
+                    string paramName = "name";
+                    throw new ArgumentException(message, paramName);
+                }
+            }
 
-            this.properties.Add(property);
+            this.properties.Add(new PropertyDefinition(name, type));
 
             return this;
         }
 
-        private Type Build()
+        public Type Build()
         {
-            // ToDo: Add implementation :)
+            MethodInfo stringEquals = typeof(String).GetMethod(
+                "op_Equality",
+                new Type[] { typeof(string), typeof(string) });
 
-            return null;
+            AppDomain domain = AppDomain.CurrentDomain;
+            AssemblyName assemblyName = new AssemblyName("DynamicAssembly");
+            AssemblyBuilderAccess access = AssemblyBuilderAccess.Run;
+            AssemblyBuilder assemblyBuilder = domain.DefineDynamicAssembly(assemblyName, access);
+            ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule("DynamicModule");
+            TypeBuilder typeBuilder = moduleBuilder.DefineType(
+                this.name,
+                TypeAttributes.Public | TypeAttributes.Sealed | TypeAttributes.Class);
+
+            // Implement IPropertyBag (GetValue / SetValue)
+            typeBuilder.AddInterfaceImplementation(typeof(IPropertyBag));
+
+            MethodBuilder getValueMethodBuilder = typeBuilder.DefineMethod(
+                "GetValue",
+                MethodAttributes.Public | MethodAttributes.Virtual | MethodAttributes.HideBySig,
+                typeof(object),
+                new Type[] { typeof(string) });
+            ILGenerator getValueILGenerator = getValueMethodBuilder.GetILGenerator();
+            MethodInfo getValueInterfaceMethod = typeof(IPropertyBag).GetMethod("GetValue");
+            typeBuilder.DefineMethodOverride(getValueMethodBuilder, getValueInterfaceMethod);
+
+            MethodBuilder setValueMethodBuilder = typeBuilder.DefineMethod(
+                "SetValue",
+                MethodAttributes.Public | MethodAttributes.Virtual | MethodAttributes.HideBySig,
+                null,
+                new Type[] { typeof(string), typeof(object) });
+            ILGenerator setValueILGenerator = setValueMethodBuilder.GetILGenerator();
+            MethodInfo setValueInterfaceMethod = typeof(IPropertyBag).GetMethod("SetValue");
+            typeBuilder.DefineMethodOverride(setValueMethodBuilder, setValueInterfaceMethod);
+
+            foreach (PropertyDefinition property in this.properties)
+            {
+                // Define the backing field for the property.
+                FieldBuilder field = typeBuilder.DefineField(
+                    "_" + property.Name,
+                    property.Type,
+                    FieldAttributes.Private);
+
+                // Define the getter method for the property.
+                MethodBuilder getterMethodBuilder = typeBuilder.DefineMethod(
+                    "get_" + property.Name,
+                    MethodAttributes.Public | MethodAttributes.HideBySig,
+                    property.Type,
+                    null);
+
+                ILGenerator getterILGenerator = getterMethodBuilder.GetILGenerator();
+                getterILGenerator.Emit(OpCodes.Ldarg_0);
+                getterILGenerator.Emit(OpCodes.Ldfld, field);
+                getterILGenerator.Emit(OpCodes.Ret);
+
+                // Define the setter method for the property.
+                MethodBuilder setterMethodBuilder = typeBuilder.DefineMethod(
+                    "set_" + property.Name,
+                    MethodAttributes.Public | MethodAttributes.HideBySig,
+                    null,
+                    new Type[] { property.Type });
+
+                ILGenerator setterILGenerator = setterMethodBuilder.GetILGenerator();
+                setterILGenerator.Emit(OpCodes.Ldarg_0);
+                setterILGenerator.Emit(OpCodes.Ldarg_1);
+                setterILGenerator.Emit(OpCodes.Stfld, field);
+                setterILGenerator.Emit(OpCodes.Ret);
+
+                // Create the property, using the generated getter and setter.
+                PropertyBuilder propertyBuilder = typeBuilder.DefineProperty(
+                    property.Name,
+                    PropertyAttributes.HasDefault,
+                    property.Type,
+                    null);
+                propertyBuilder.SetGetMethod(getterMethodBuilder);
+                propertyBuilder.SetSetMethod(setterMethodBuilder);
+
+                // Add the resolution of this property to the GetValue method.
+                // The rough intent of this code is to generate something similar to:
+                //   if (String.Equals(ARGUMENT_PROPERTY_NAME, INLINED_PROPERTY_NAME))
+                //   {
+                //       return FIELD_VALUE;
+                //   }
+                Label getValueSkipLabel = getValueILGenerator.DefineLabel();
+                getValueILGenerator.Emit(OpCodes.Ldarg_1);
+                getValueILGenerator.Emit(OpCodes.Ldstr, property.Name);
+                getValueILGenerator.EmitCall(OpCodes.Call, stringEquals, null);
+                getValueILGenerator.Emit(OpCodes.Brfalse_S, getValueSkipLabel);
+                getValueILGenerator.Emit(OpCodes.Ldarg_0);
+                getValueILGenerator.Emit(OpCodes.Ldfld, field);
+                if (property.Type.IsValueType)
+                    getValueILGenerator.Emit(OpCodes.Box, property.Type);
+                getValueILGenerator.Emit(OpCodes.Ret);
+                getValueILGenerator.MarkLabel(getValueSkipLabel);
+
+                // Adds this property to the SetValue method.
+                // The rough intent of this code is to generate something similar to:
+                //   if (String.Equals(ARGUMENT_PROPERTY_NAME, INLINED_PROPERTY_NAME))
+                //   {
+                //       FIELD_VALUE = ARGUMENT_FIELD_VALUE;
+                //       return;
+                //   }
+                Label setValueSkipLabel = setValueILGenerator.DefineLabel();
+                setValueILGenerator.Emit(OpCodes.Ldarg_1);
+                setValueILGenerator.Emit(OpCodes.Ldstr, property.Name);
+                setValueILGenerator.EmitCall(OpCodes.Call, stringEquals, null);
+                setValueILGenerator.Emit(OpCodes.Brfalse_S, setValueSkipLabel);
+                setValueILGenerator.Emit(OpCodes.Ldarg_0);
+                setValueILGenerator.Emit(OpCodes.Ldarg_2);
+                if (property.Type.IsValueType)
+                    setValueILGenerator.Emit(OpCodes.Unbox_Any, property.Type);
+                else
+                    setValueILGenerator.Emit(OpCodes.Castclass, property.Type);
+                setValueILGenerator.Emit(OpCodes.Stfld, field);
+                setValueILGenerator.Emit(OpCodes.Ret);
+                setValueILGenerator.MarkLabel(setValueSkipLabel);
+            }
+
+            // If the GetValue and SetValue methods have made it past all of the properties
+            // without a match (and thus a return), we'll throw an ArgumentException.
+            ConstructorInfo exConstructor = typeof(ArgumentException)
+                .GetConstructor(new Type[] { typeof(string), typeof(string) });
+            string exMessage = "propertyName must refer to a parameter that exists on this type.";
+            string exParameterName = "propertyName";
+            getValueILGenerator.Emit(OpCodes.Ldstr, exMessage);
+            getValueILGenerator.Emit(OpCodes.Ldstr, exParameterName);
+            getValueILGenerator.Emit(OpCodes.Newobj, exConstructor);
+            getValueILGenerator.Emit(OpCodes.Throw);
+            setValueILGenerator.Emit(OpCodes.Ldstr, exMessage);
+            setValueILGenerator.Emit(OpCodes.Ldstr, exParameterName);
+            setValueILGenerator.Emit(OpCodes.Newobj, exConstructor);
+            setValueILGenerator.Emit(OpCodes.Throw);
+
+            // Build and return the type!
+            Type generatedType = typeBuilder.CreateType();
+            return generatedType;
         }
     }
 }
